b5869f406c0d676fc804b55abb4ca789c92df5c0
angelomoraes <angeloaugustomoraes@gmail.com>
2024-11-12 12:47:34 -0300

    att

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'Quem é a melhor professora de tecnologia?',
+    'Mirela'
+)
+
+criaCartao(
+    'Química',
+    'Quem é a maior conhecedora de química?',
+    'Mirela'
+)
+
+criaCartao(
+    'Curiosidades Gerais',
+    'Em uma briga entre todos os professores, quem venceria?',
+    'Provavelmente Mirela'
+)
